29c097235e0f045bd7463432e98f459368834681
Janning96 <janning.fliegel@gmx.de>
2025-08-21 08:54:14 +0200

    Add files via upload

---
+++ docs/app.js
@@ -0,0 +1,2 @@
+// placeholder app.js v6.3 (Implementierung bleibt wie vorher, inkl. ensurePDFReady und Import-Logik)
+console.log('App v6.3 geladen');
---
+++ docs/service-worker.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e=>{e.waitUntil(caches.open('kosten-tracker-v6.3').then(c=>c.addAll(['./','./index.html'])))});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
